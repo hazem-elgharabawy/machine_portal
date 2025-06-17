7eb8e62d7f7b59b904ccb554bf95f74a89917637
HazemElgharabawy <hazem.gharabawy6@gmail.com>
2025-06-17 19:32:23 +0300

    ce_single 16bit accumlating yhy11

--- CE_single_final/inc/fixed.h
+++ CE_single_final/inc/fixed.h
@@ -7,7 +7,7 @@
 
 
 // Fixed-point configuration
-#define INT_WIDTH 6
+#define INT_WIDTH 8
 #define FRACT_WIDTH 10
 #define DATA_WIDTH INT_WIDTH + FRACT_WIDTH    
 #define PRODUCT_DATA_WIDTH 2*DATA_WIDTH
@@ -21,57 +21,42 @@ typedef int32_t fixed_point_t;
 typedef int64_t fixed_point_product_t;
 #endif
 
+// Helper function for printing bits of fixed-point numbers
+static inline void print_bits(fixed_point_t num) {
+    for (int i = DATA_WIDTH-1; i >= 0; i--) { // Start from the most significant bit
+        printf("%d", (num >> i) & 1); // Extract and print each bit
+        if (i % 4 == 0) { // Optional: Add a space for readability every 4 bits
+            printf(" ");
+        }
+    }
+    printf("\n");
+}
+
+// Helper function for printing bits of fixed-point product numbers
+static inline void print_bits_product(fixed_point_product_t num) {
+    for (int i = PRODUCT_DATA_WIDTH-1 ; i >= 0; i--) { // Start from the most significant bit
+        printf("%d", (num >> i) & 1); // Extract and print each bit
+        if (i % 4 == 0) { // Optional: Add a space for readability every 4 bits
+            printf(" ");
+        }
+    }
+    printf("\n");
+}
+
 // Helper function for fixed-point multiplication
-
-static inline void print_bits_16(fixed_point_t num) {
-    for (int i = 15; i >= 0; i--) { // Start from the most significant bit
-        printf("%d", (num >> i) & 1); // Extract and print each bit
-        if (i % 4 == 0) { // Optional: Add a space for readability every 4 bits
-            printf(" ");
-        }
-    }
-    printf("\n");
-}
-static inline void print_bits_32(fixed_point_product_t num) {
-    for (int i = 31; i >= 0; i--) { // Start from the most significant bit
-        printf("%d", (num >> i) & 1); // Extract and print each bit
-        if (i % 4 == 0) { // Optional: Add a space for readability every 4 bits
-            printf(" ");
-        }
-    }
-    printf("\n");
-}
-
 static inline fixed_point_t fixed_mul(fixed_point_t a, fixed_point_t b) {
-    fixed_point_product_t temp = a * b;
-    /*
-    printf("INSIDE FIXED MUL \n");
-    printf("a = ");
-    print_bits(a);
-    printf("b = ");
-    print_bits(b);
-    printf("Product ");
-    print_bits_product(temp);
-    */
-    return (fixed_point_t) (temp >> FRACT_WIDTH);
+    return (fixed_point_t) (((fixed_point_product_t)a * b) >> FRACT_WIDTH);
 }
 
 // Helper function for fixed-point division
 static inline fixed_point_t fixed_div(fixed_point_t a, fixed_point_t b) {
     if (b == 0) return 0;
-    return (fixed_point_t)((fixed_point_product_t)a << FRACT_WIDTH) / b;
-}
-
-// Helper function for fixed-point multiplication with addition
-static inline fixed_point_t fixed_mul_4(fixed_point_t a, fixed_point_t b, fixed_point_t c, fixed_point_t d){
-    fixed_point_product_t product = ((fixed_point_product_t)a * (fixed_point_product_t)b) + ((fixed_point_product_t)c * (fixed_point_product_t)d);
-    product = product >> FRACT_WIDTH;
-    return (fixed_point_t)product;
+    return (fixed_point_t) (((fixed_point_product_t)a << FRACT_WIDTH) / b);
 }
 
 // Converts a double to a fixed-point number
 inline fixed_point_t double_to_fixed(double value) {    
-    return (fixed_point_t)(value * (1 << FRACT_WIDTH) + 0.5);  // Multiply by 2^FRACT_WIDTH and round
+    return (fixed_point_t)(value * (1 << FRACT_WIDTH));  // Multiply by 2^FRACT_WIDTH and round
 }
 
 // Converts a fixed-point number to a double
---
+++ CE_single_final/src/main.c
@@ -0,0 +1,176 @@
+#include "../inc/main.h"
+
+int main(){
+    Actuator_S actuator = {0};
+    actuator.a10_r = double_to_fixed(1);
+
+    fixed_point_t yhy_11_acumalator_arr[SET_SIZE/100] = {0};
+    fixed_point_t yhy_11  = 0;
+    fixed_point_t yhy_12  = 0;
+    fixed_point_t yhy_13  = 0;
+    fixed_point_t yhy_23  = 0;
+    fixed_point_t yhy_33  = 0;
+
+    fixed_point_t yhe_1_r = 0;
+    fixed_point_t yhe_1_i = 0;
+    fixed_point_t yhe_2_r = 0;
+    fixed_point_t yhe_2_i = 0;
+    fixed_point_t yhe_3_r = 0;
+    fixed_point_t yhe_3_i = 0;
+
+    Actuator_S actuator = {0};
+    actuator.a10_r = double_to_fixed(1);     // (no distortion) for the first block of data  
+
+    printf("before for loop\n");
+    // compute YHY & YHE in the same for loop to remove redundant operations
+    for (int i = 0; i < SET_SIZE; i ++ ) {
+        
+        //load from arrays to temp
+        fixed_point_t y_r_temp = sync_pa_out_r_arr[i];
+        fixed_point_t y_i_temp = sync_pa_out_i_arr[i];
+        
+        fixed_point_t yp_r_temp = post_dist_out_r_arr[i];
+        fixed_point_t yp_i_temp = post_dist_out_i_arr[i];
+        
+        fixed_point_t xp_r_temp = sync_pa_in_r_arr[i];
+        fixed_point_t xp_i_temp = sync_pa_in_i_arr[i];
+        
+        ///////////////////////Post Distorter///////////////////////
+        
+        // Load input 
+        actuator.in_r = y_r_temp;
+        actuator.in_i = y_i_temp;
+    
+        // Perform DPD operation
+        actuator_func(&actuator);
+
+        // Y absolute squared and power 4 for both YHY & YHE
+        fixed_point_t in_r_2 = fixed_mul(y_r_temp,y_r_temp);
+        fixed_point_t in_i_2 = fixed_mul(y_i_temp,y_i_temp);
+        fixed_point_t y_2_temp =  in_r_2 + in_i_2;
+        fixed_point_t y_4_temp = fixed_mul(y_2_temp,y_2_temp);
+        
+        // for YHY 
+        fixed_point_t y_6_temp = fixed_mul(y_2_temp,y_4_temp);
+        fixed_point_t y_8_temp = fixed_mul(y_4_temp,y_4_temp);
+        fixed_point_t y_10_temp = fixed_mul(y_4_temp,y_6_temp);
+
+        // for YHE
+        //error for YHE   e = x (pre-distorted) - y (post-distorted) 
+        fixed_point_t e_r = xp_r_temp - actuator.out_r;
+        fixed_point_t e_i = xp_i_temp - actuator.out_i;
+        
+        // y* e =  (yr - j yi) (er + j ei) = (yr er + yi ei ) + j (yr ei - yi er)
+        fixed_point_t temp_r = fixed_mul(y_r_temp, e_r) + fixed_mul(y_i_temp, e_i); 
+        fixed_point_t temp_i = fixed_mul(y_r_temp, e_i) - fixed_mul(y_i_temp, e_r); 
+        
+        // accumulate the YHY and YHE values
+        //YHY
+        yhy_11 += y_2_temp;
+        yhy_12 += y_4_temp;
+        yhy_13 += y_6_temp; 
+        yhy_23 += y_8_temp;
+        yhy_33 += y_10_temp;
+        if (i != 0 && i % 100 == 0) {
+            yhy_11_acumalator_arr[i / 100 - 1] = yhy_11;
+            yhy_11 = 0;
+        }
+
+        
+        //YHE
+        yhe_1_r += temp_r;
+        yhe_1_i += temp_i; 
+        yhe_2_r += fixed_mul(y_2_temp,temp_r);
+        yhe_2_i += fixed_mul(y_2_temp,temp_i);
+        yhe_3_r += fixed_mul(y_4_temp,temp_r);
+        yhe_3_i += fixed_mul(y_4_temp,temp_i);
+
+    }
+
+    for (size_t i = 0; i < (SET_SIZE /100); i++)
+    {
+        yhy_11 += yhy_11_acumalator_arr[i];
+    }
+    
+    
+
+    // now we have all the YHY and YHE values accumulated
+    // We need to calculate the YHY inverse matrix and the YHE matrix
+
+/*
+*********************************************************************************************************
+yhe_mat is a 3x1 matrix with the following structure:
+
+hye_mat =  
+    | yhe1r + yhe1i |
+    | yhe2r + yhe2i |
+    | yhe3r + yhe3i |
+                    3x1 matrix
+
+
+HOWEVER  we will save it in a 3x3 symmetric matrix just to use the same structure Matrix_S for simplicity
+    | yhe1r |  | yhe1i |  | yhe2r |
+    | yhe1i |  | yhe2i |  | yhe3r |
+    | yhe2r |  | yhe3r |  | yhe3i |
+where:
+    yhe_mat-> a11 = yhe1r
+    yhe_mat-> a12 = yhe1i               *****THIS DOES NOT HAVE ANY MEANING, IT IS JUST A PLACEHOLDER*****
+    yhe_mat-> a13 = yhe2r
+    yhe_mat-> a22 = yhe2i
+    yhe_mat-> a23 = yhe3r
+    yhe_mat-> a33 = yhe3i
+************************************************************************************************************
+*/
+    // YHE matrix
+    // Note: We are using a 3x3 matrix to store the YHE values
+    Matrix_S yhe_mat = {0};
+    yhe_mat.a11 = yhe_1_r;
+    yhe_mat.a12 = yhe_1_i;
+    yhe_mat.a13 = yhe_2_r;
+    yhe_mat.a22 = yhe_2_i;
+    yhe_mat.a23 = yhe_3_r;
+    yhe_mat.a33 = yhe_3_i;
+
+
+
+    // YHY  matrix
+    // Note: We are using a 3x3 matrix to store the YHY values
+    // The YHY matrix is symmetric, so we only need to store the upper triangular part
+    // NOTE: the YHY values are multiplied by 0.25 (1/4) before inverting 
+    Matrix_S yhy_matrix = {0};
+    yhy_matrix.a11 = (yhy_11 >> 2);
+    yhy_matrix.a12 = (yhy_12 >> 2);
+    yhy_matrix.a13 = (yhy_13 >> 2);
+    yhy_matrix.a23 = (yhy_23 >> 2);
+    yhy_matrix.a33 = (yhy_33 >> 2);
+
+    Matrix_S yhy_inv = {0};
+    
+    int yhy_inv_status = mat_inv(&yhy_matrix, &yhy_inv);  
+    if (yhy_inv_status != 0) {
+        printf("Error: YHY matrix inversion failed (det = 0) \n");
+        return yhy_inv_status;
+    }
+
+    // Now we have the YHY inverse matrix, we need to scale it by 16
+    yhy_inv.a11 = yhy_inv.a11 << 4; 
+    yhy_inv.a12 = yhy_inv.a12 << 4; 
+    yhy_inv.a13 = yhy_inv.a13 << 4; 
+    yhy_inv.a22 = yhy_inv.a22 << 4; 
+    yhy_inv.a23 = yhy_inv.a23 << 4; 
+    yhy_inv.a33 = yhy_inv.a33 << 4; 
+
+    //update coefficients of the distorter
+    coeff_update(&yhy_inv, &yhe_mat, &actuator);
+
+
+    printf("after coeff_update\n");
+    printf("actuator.a10_r = %lf\n", fixed_to_double(actuator.a10_r));  
+    printf("actuator.a10_i = %lf\n", fixed_to_double(actuator.a10_i));
+    printf("actuator.a30_r = %lf\n", fixed_to_double(actuator.a30_r));
+    printf("actuator.a30_i = %lf\n", fixed_to_double(actuator.a30_i));
+    printf("actuator.a50_r = %lf\n", fixed_to_double(actuator.a50_r));
+    printf("actuator.a50_i = %lf\n", fixed_to_double(actuator.a50_i));
+
+    return 0;
+}
